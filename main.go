20ba21a722f6e942d5d1730aa07c518d88426c49
RunAtTekky <varunrawat343@gmail.com>
2026-06-09 16:18:41 +0530

    cleanup main.go

--- main.go
+++ main.go
@@ -9,17 +9,11 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-var game_over bool = false
-var board [3][3]rune
-var x_turn bool = true
-
 func main() {
-
 	p := tea.NewProgram(ui.InitialModel())
 
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("There was an error %v", err)
 		os.Exit(1)
 	}
-
 }
