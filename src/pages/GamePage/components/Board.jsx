346c8bd81989bd6efb9617e10f8b1807025b1ae4
RunAtTekky <varunrawat343@gmail.com>
2026-06-12 19:46:32 +0530

    turn changes

--- src/pages/GamePage/components/Board.jsx
+++ src/pages/GamePage/components/Board.jsx
@@ -6,16 +6,20 @@ import { Cell } from "./Cell";
 
 export const Board = () => {
   const [cells, setCells] = useState(Array(9).fill(BOARD.EMPTY));
+  const [xTurn, setXTurn] = useState(true);
 
-  const onCellClick = (idx) => {
-    console.log("Button clicked");
+  const handleCellClick = (idx) => {
     const newCells = cells.slice();
-    newCells[idx] = BOARD.X_SYMBOL;
+    if (newCells[idx] != BOARD.EMPTY) return;
+
+    newCells[idx] = (xTurn) ? BOARD.X_SYMBOL : BOARD.O_SYMBOL;
     setCells(newCells);
+
+    setXTurn(!xTurn);
   };
 
   const cellsBtn = cells.map((cell, idx) => (
-    <Cell key={idx} value={cell} onClick={onCellClick} idx={idx} />
+    <Cell key={idx} value={cell} onClick={handleCellClick} idx={idx} />
   ));
 
   return (
