8b5c2b5eac910e95498ace5868aaf42a2357b26c
RunAtTekky <varunrawat343@gmail.com>
2026-06-26 20:15:05 +0530

    Highlight the winning lines when game ends
    Give user an easier experience to identify the winning lines

--- src/pages/GamePage/GamePage.helpers.js
+++ src/pages/GamePage/GamePage.helpers.js
@@ -1,3 +1,5 @@
+import { BOARD } from "../../constants";
+
 export const getRowCol = (idx) => {
   const row = Math.trunc(idx / 3);
   const col = idx % 3;
@@ -7,6 +9,61 @@ export const getRowCol = (idx) => {
     col,
   };
 };
+
+export const getWinningHighlights = (cells) => {
+  const isHighlighted = {};
+
+  const highlightedIdxs = getHighlightedIdxs(cells);
+
+  for (let idx=0; idx<9; idx++) {
+    isHighlighted[idx] = false;
+  }
+
+  for (let idx of highlightedIdxs) {
+    isHighlighted[idx] = true;
+  }
+
+  console.log(isHighlighted);
+
+  return isHighlighted;
+}
+
+const getHighlightedIdxs = (cells) => {
+  let highlightedIdxs = [];
+
+  // Horizontal
+  for (let i=0; i<=6; i+=3) {
+    if (cells[i] !== BOARD.EMPTY && cells[i] === cells[i+1] && cells[i] === cells[i+2]) {
+      highlightedIdxs.push(i);
+      highlightedIdxs.push(i+1);
+      highlightedIdxs.push(i+2);
+    }
+  }
+
+  // Vertical
+  for (let i=0; i<3; i++) {
+    if (cells[i] !== BOARD.EMPTY && cells[i] === cells[i+3] && cells[i] === cells[i+6]) {
+      highlightedIdxs.push(i);
+      highlightedIdxs.push(i+3);
+      highlightedIdxs.push(i+6);
+    }
+  }
+
+  // Diagonal
+  if (cells[0] !== BOARD.EMPTY && cells[0] === cells[4] && cells[0] === cells[8]) {
+    highlightedIdxs.push(0);
+    highlightedIdxs.push(4);
+    highlightedIdxs.push(8);
+  }
+
+  if (cells[2] !== BOARD.EMPTY && cells[2] === cells[4] && cells[2] === cells[6]) {
+    highlightedIdxs.push(2);
+    highlightedIdxs.push(4);
+    highlightedIdxs.push(6);
+  }
+  
+  return highlightedIdxs;
+}
 
 export const getIdx = (row, col) => {
   const idx = row * 3 + col;
